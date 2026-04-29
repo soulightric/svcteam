209f27264456625c762a7d1c42816e768b20b546
Fikri Sujud <fikrizeronine@gmail.com>
2026-04-29 21:19:44 +0800

    feat: ops for page

---
+++ app/ops/page.tsx
@@ -0,0 +1,11 @@
+export const metadata: metadata = {
+    title: "Ops";
+}
+
+export default function Ops () {
+    return(
+        <main className="flex h-screen justify-center">
+            <h1 className="text-2xl">Laravel backend</h1>
+        </main>
+    );
+}
